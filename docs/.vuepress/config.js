49b49d866eed3e7562b8d828cf193f660145d484
Firmansyah Mukti Wijaya <iki.mukti@gmail.com>
2025-03-11 14:46:50 +0700

    dfdsfsdf

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,151 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  lang: 'en-US',
+  lang: 'id',
 
-  title: 'VuePress',
-  description: 'My first VuePress Site',
+  title: 'Himastat UT Docs',
+  description: 'Himpunan Mahasiswa Statistika Universitas Terbuka',
+  head: [
+    [
+      'meta',
+      {
+        name: 'Himastat-UT Team | Ikimukti',
+        content: 'Himastat-UT Team 2024/2025'
+      }
+    ]
+  ],
 
   theme: defaultTheme({
-    logo: 'https://vuejs.press/images/hero.png',
+    logo: 'public/images/logo.png',
 
-    navbar: ['/', '/get-started'],
+    navbar: [
+      // NavbarLink for Home
+      {
+        text: 'Home',
+        link: '/',
+      },
+      // NavbarGroup for StatGen with all the courses
+      {
+        text: 'StatGen',
+        children: [
+          {
+            text: 'Semester 1',
+            prefix: '/statgen/',
+            children: [
+              { text: 'BIOLOGI UMUM', link: 'BIOL4110/' },
+              { text: 'KIMIA DASAR I', link: 'KIMD4110/' },
+              { text: 'KALKULUS I', link: 'MATA4110/' },
+              { text: 'ILMU SOSIAL DAN BUDAYA DASAR', link: 'MKDU4109/' },
+              { text: 'BAHASA INGGRIS', link: 'MKWI4201/' },
+              { text: 'PENDIDIKAN KEWARGANEGARAAN', link: 'MKWU4109/' },
+              { text: 'METODE STATISTIK I', link: 'SATS4121/' },
+            ],
+          },
+          {
+            text: 'Semester 2',
+            prefix: '/statgen/',
+            children: [
+              { text: 'FISIKA DASAR I', link: 'FISD4211/' },
+              { text: 'PENGANTAR SOSIOLOGI', link: 'ISIP4110/' },
+              { text: 'BAHASA INDONESIA', link: 'MKWU4108/' },
+              { text: 'KOMPUTER I', link: 'SATS4111/' },
+              { text: 'MATEMATIKA I', link: 'SATS4120/' },
+              { text: 'METODE STATISTIK II', link: 'SATS4211/' },
+              { text: 'ANALISIS DATA STATISTIK', link: 'SATS4212/' },
+            ],
+          },
+          {
+            text: 'Semester 3',
+            prefix: '/statgen/',
+            children: [
+              { text: 'PENDIDIKAN AGAMA ISLAM', link: 'MKWU4101/' },
+              { text: 'ALJABAR LINEAR TERAPAN', link: 'SATS4122/' },
+              { text: 'MATEMATIKA II', link: 'SATS4210/' },
+              { text: 'PENGUMPULAN DAN PENYAJIAN DATA', link: 'SATS4213/' },
+              { text: 'PENGANTAR PROBABILITAS', link: 'SATS4221/' },
+              { text: 'KOMPUTER II', link: 'SATS4223/' },
+              { text: 'STATISTIKA PENGAWASAN KUALITAS', link: 'SATS4310/' },
+            ],
+          },
+          {
+            text: 'Semester 4',
+            prefix: '/statgen/',
+            children: [
+              { text: 'PENGANTAR EKONOMI MAKRO', link: 'ESPA4110/' },
+              { text: 'MATEMATIKA III', link: 'SATS4220/' },
+              { text: 'RANCANGAN PERCOBAAN', link: 'SATS4222/' },
+              { text: 'PENGANTAR SOSIOMETRI', link: 'SATS4224/' },
+              { text: 'MODEL LINEAR TERAPAN', link: 'SATS4312/' },
+              { text: 'DEMOGRAFI', link: 'SATS4313/' },
+              { text: 'PENGANTAR PROSES STOKASTIK', link: 'SATS4322/' },
+            ],
+          },
+          {
+            text: 'Semester 5',
+            prefix: '/statgen/',
+            children: [
+              { text: 'SISTEM INFORMASI MANAJEMEN', link: 'ADPU4442/' },
+              { text: 'RISET OPERASIONAL I', link: 'MATA4343/' },
+              { text: 'ASURANSI I', link: 'SATS4311/' },
+              { text: 'METODE SAMPLING', link: 'SATS4321/' },
+              { text: 'METODE PERAMALAN', link: 'SATS4323/' },
+              { text: 'PENGANTAR STATISTIKA MATEMATIS I', link: 'SATS4410/' },
+              { text: 'METODE STATISTIKA NON PARAMETRIK', link: 'SATS4411/' },
+            ],
+          },
+          {
+            text: 'Semester 6',
+            prefix: '/statgen/',
+            children: [
+              { text: 'TEORI PEMBUATAN KEPUTUSAN', link: 'ADBI4531/' },
+              { text: 'METODOLOGI PENELITIAN', link: 'MSIM4312/' },
+              { text: 'ASURANSI II', link: 'SATS4412/' },
+              { text: 'PENGANTAR STATISTIKA MATEMATIS II', link: 'SATS4420/' },
+              { text: 'METODE STATISTIKA MULTIVARIAT', link: 'SATS4421/' },
+              { text: 'METODE SEKUENSIAL', link: 'SATS4422/' },
+            ],
+          },
+          {
+            text: 'Semester 7',
+            prefix: '/statgen/',
+            children: [
+              { text: 'INFERENSI BAYESIAN', link: 'SATS4324/' },
+              { text: 'ANALISIS RUNTUN WAKTU', link: 'SATS4423/' },
+              { text: 'TUGAS AKHIR PROGRAM', link: 'SATS4500/' },
+              { text: 'KARYA ILMIAH', link: 'SATS4560/' },
+            ],
+          },
+          {
+            text: 'Semester 8',
+            prefix: '/statgen/',
+            children: [
+              { text: 'MANAJEMEN', link: 'EKMA4116/' },
+              { text: 'PENGANTAR EKONOMI MIKRO', link: 'ESPA4111/' },
+              { text: 'EKONOMETRIKA', link: 'ESPA4312/' },
+            ],
+          },
+        ],
+      },
+      // Other links
+      {
+        text: 'Radar',
+        link: '/radar/',
+      },
+      {
+        text: 'Gemas',
+        link: '/gemas/',
+      },
+      {
+        text: 'Link',
+        link: '/link/',
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/arbiet/himastatut_vuepress',
+        target: '_self',
+        rel: false,
+      },
+    ],
   }),
 
   bundler: viteBundler(),
--- docs/README.md
+++ docs/README.md
@@ -1,31 +1,40 @@
 ---
 home: true
 title: Home
-heroImage: https://vuejs.press/images/hero.png
+heroImage: public/images/logo.png
+tagline: Cerdas Bersama Himastat UT
 actions:
-  - text: Get Started
-    link: /getting-started.html
+  - text: Statistics Generation
+    link: /statgen-started.html
     type: primary
 
-  - text: Introduction
-    link: https://vuejs.press/guide/introduction.html
+  - text: Pelajari Lebih Lanjut
+    link: /intro.html
     type: secondary
 
 features:
-  - title: Simplicity First
-    details: Minimal setup with markdown-centered project structure helps you focus on writing.
-  - title: Vue-Powered
-    details: Enjoy the dev experience of Vue, use Vue components in markdown, and develop custom themes with Vue.
-  - title: Performant
-    details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
-  - title: Themes
-    details: Providing a default theme out of the box. You can also choose a community theme or create your own one.
-  - title: Plugins
-    details: Flexible plugin API, allowing plugins to provide lots of plug-and-play features for your site.
-  - title: Bundlers
-    details: Default bundler is Vite, while Webpack is also supported. Choose the one you like!
+  - title: HANGAT (Hubungan Aktif dan Komunikasi Terpadu)
+    details: Program ini membentuk saluran WhatsApp untuk mahasiswa Statistika per daerah dan mendukung kegiatan resmi. Dari 50 grup, 39 dialokasikan untuk mahasiswa per daerah, sementara sisanya digunakan untuk acara atau kegiatan resmi Statistika Universitas Terbuka.
 
-footer: MIT Licensed | Copyright © 2018-present VuePress Community
+  - title: RADAR (Rapat Asyik Diskusi dan Analisis Review)
+    details: Program evaluasi pengurus dilaksanakan 1x sebulan untuk meninjau kinerja dan perkembangan kegiatan organisasi, serta mendiskusikan strategi peningkatan efektivitas himpunan.
+
+  - title: MELANJUTKAN STATGEN
+    details: Program ini memberikan bimbingan statistik pada 2-4 mata kuliah per semester, membantu mahasiswa memahami materi dengan lebih mendalam dan mempersiapkan diri menghadapi ujian serta tugas akademik.
+
+  - title: GEMAS (Gerakan Mempersembahkan Inspirasi dan Apresiasi)
+    details: Program ini bertujuan memberikan penghargaan kepada mahasiswa yang berprestasi dan alumni yang telah menyelesaikan studi. Dilaksanakan sebagai bentuk apresiasi dan motivasi, acara ini juga memperkuat hubungan antara himpunan, mahasiswa, pengurus lama/baru hima, dan alumni.
+
+  - title: JEPRET (Jejak Pengurus di Feed Terupdate)
+    details: Program yang bertujuan untuk memberikan apresiasi kepada pengurus lama atas kontribusi mereka selama masa kepengurusan. Selain itu, program ini juga menyusun dan mempublikasikan informasi tentang susunan kepengurusan HIMASTAT yang baru sebagai bentuk transparansi dan informasi kepada anggota.
+
+  - title: LINK (Lintasan Inspirasi Nurturing Kegiatan)
+    details: Program ini bertujuan untuk mempertemukan mahasiswa agar dapat mempersiapkan diri dalam mengikuti lomba-lomba terkait statistik dan membentuk tim serta memberikan pelatihan intensif.
+
+  - title: TAKTIK (Temu Aksi Kreatif untuk Tim dan Kompetisi)
+    details: Program ini dirancang untuk meningkatkan kreativitas dan kompetisi di antara mahasiswa melalui berbagai kegiatan yang melibatkan kolaborasi dan inovasi.
+
+footer: MIT Licensed | Copyright © 2024-present Himpunan Mahasiswa Universitas Terbuka
 ---
 
 This is the content of home page. Check [Home Page Docs][default-theme-home] for more details.
